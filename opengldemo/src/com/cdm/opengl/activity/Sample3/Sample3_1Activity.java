7457347fba5e17f2e7b665b072e3bf2a8106eb39
cdm <532456718@qq.com>
2013-08-22 21:43:19 +0800

    Signed-off-by: cdm <532456718@qq.com>

--- opengldemo/src/com/cdm/opengl/activity/Sample3/Sample3_1Activity.java
+++ opengldemo/src/com/cdm/opengl/activity/Sample3/Sample3_1Activity.java
@@ -1,26 +1,35 @@
 package com.cdm.opengl.activity.Sample3;
 
+import com.cdm.opengl.view.MyTDView;
+
 import android.app.Activity;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
 public class Sample3_1Activity extends Activity {
 
+	MyTDView mview;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
+		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+		mview = new MyTDView(this);
+		mview.requestFocus();
+		mview.setFocusableInTouchMode(true);
+		setContentView(mview);
 	}
 
 	@Override
 	protected void onResume() {
-		// TODO Auto-generated method stub
 		super.onResume();
+		mview.onResume();
 	}
 
 	@Override
 	protected void onPause() {
-		// TODO Auto-generated method stub
 		super.onPause();
+		mview.onPause();
 	}
 	
 	
