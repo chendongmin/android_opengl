237f8e917c141b5bf4a991179fefbfecd3454b39
cdm <532456718@qq.com>
2013-08-31 16:12:42 +0800

    Signed-off-by: cdm <532456718@qq.com>

--- opengldemo/src/com/cdm/opengl/util/MatrixState.java
+++ opengldemo/src/com/cdm/opengl/util/MatrixState.java
@@ -1,13 +1,43 @@
 package com.cdm.opengl.util;
 
-import android.opengl.GLES20;
 import android.opengl.Matrix;
 
 public class MatrixState {
 	
-	private static float[] mProjMatrix = new float[16];
-	private static float[] mVMatrix = new float[16];
-	private static float[] mMVPMatrix ;
+	private static float[] mProjMatrix = new float[16];//投影
+	private static float[] mVMatrix = new float[16];//摄像机朝向
+	private static float[] currMatrix;
+	private static float[] mMVPMatrix  = new float[16]; ;
+	static float[][] mStack = new float[10][16];
+	static int stackTop = -1;
+	
+	
+	public static void setInitStack(){
+		currMatrix = new float[16];
+		Matrix.setRotateM(currMatrix, 0, 0, 1, 0, 0);
+	}
+	
+	public static void pushMatrix(){
+		stackTop++;
+		for(int i=0;i<16;i++){
+			mStack[stackTop][i]=currMatrix[i];
+		}
+	}
+	
+	public static void popMatrix(){
+		for(int i=0;i<16;i++){
+			currMatrix[i] = mStack[stackTop][i];
+			stackTop--;
+		}
+	}
+	
+	
+	public static void translate(float x,float y,float z){
+		Matrix.translateM(currMatrix, 0, x, y, z);
+	}
+	
+	
+	
 	
 	public static void setCamera(float cx,float cy,float cz,
 			float tx,float ty,float tz,
@@ -24,9 +54,18 @@ public class MatrixState {
 	}
 	
 	public static float[] getFinalMatrix(float[] spec){
-		mMVPMatrix = new float[16];
+		
 		//将摄像机矩阵乘以变换矩阵
 		Matrix.multiplyMM(mMVPMatrix, 0, mVMatrix, 0, spec, 0);
+		//
+		Matrix.multiplyMM(mMVPMatrix, 0, mProjMatrix, 0, mMVPMatrix, 0);
+		return mMVPMatrix;
+	}
+	
+	public static float[] getFinalMatrix(){
+		
+		//将摄像机矩阵乘以变换矩阵
+		Matrix.multiplyMM(mMVPMatrix, 0, mVMatrix, 0, currMatrix, 0);
 		//
 		Matrix.multiplyMM(mMVPMatrix, 0, mProjMatrix, 0, mMVPMatrix, 0);
 		return mMVPMatrix;
