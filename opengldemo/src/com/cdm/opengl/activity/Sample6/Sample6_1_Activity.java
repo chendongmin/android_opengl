dfcad3e94170bcdf1b60f787576a40249aa96db7
cdm <532456718@qq.com>
2013-09-04 16:19:12 +0800

    Signed-off-by: cdm <532456718@qq.com>

---
+++ opengldemo/src/com/cdm/opengl/activity/Sample6/Sample6_1_Activity.java
@@ -0,0 +1,35 @@
+package com.cdm.opengl.activity.Sample6;
+import com.cdm.opengl.view.Sample6.MySurfaceView;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class Sample6_1_Activity extends Activity {
+	private MySurfaceView mGLSurfaceView;
+    @Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		// 设置为全屏
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		// 初始化GLSurfaceView
+		mGLSurfaceView = new MySurfaceView(this);
+		// 切换到主界面
+		setContentView(mGLSurfaceView);	
+	}
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mGLSurfaceView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mGLSurfaceView.onPause(); 
+    } 
+}
